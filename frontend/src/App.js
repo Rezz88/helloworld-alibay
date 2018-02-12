74217d66e95715e120836b40ad3a6d36e34702de
Rezz88 <zzerep.88@hotmail.com>
2018-02-12 13:03:29 -0500

    Confirm push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     super();
     this.state = {
       active: '',
-      login: true,
+      login: true, //Temp marked as true
       error: true
     }
   }
@@ -72,21 +72,22 @@ class App extends Component {
     })
       .then(x => x.text())
       .then(x => JSON.parse(x))
-      .then(x => this.setState({ 
+      .then(x => this.setState({
         login: x,
         error: !x
-       }).catch((err)=> {
-         console.log(err)
-         this.setState({error: true})
-        })
+      }).catch((err) => {
+        console.log(err)
+        this.setState({ error: true })
+      })
       )   //change x to something more descriptive
+
   }
 
   render() {
     // const { active } = this.state;
     return (
       <div>
-        <ul className = "App-header">
+        <ul className="App-header">
           <li>
             <a onClick={() => this.ChangeComponent('Main')}>M A I N</a>
           </li>
@@ -106,59 +107,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-
-
-
-
-// import React, { Component } from 'react';
-// // import logo from './logo.svg';
-// import './App.css';
-// import {Main} from './main.js'
-
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       loggedin: false, 
-//       mainstate: true,
-//       // loadMain: false,
-//       // loadProfile: false
-//     }
-//   }
-
-//   // ChangeMainState = (newValue) => {
-//   //   this.setState({loadMain : newValue})
-//   // } 
-
-//   render() {
-
-//     if (!this.state.loggedin) {
-//       return  (
-//        <Main
-//        mainState = {this.mainstate}
-//       //  ChangeLoadMain = {this.ChangeMainState}
-//        /> 
-//       )
-//     }
-//   }
-// }
-
-// export default App;
-
-
-// if (this.state.loadMain===true)  {
-    // return (
-    // <Main
-    // changeParentState = {this.ChangeAppState}
-    // />
-    // )
-    // }
-    // if (this.state.loadMain === false)  {
-    //   return (
-    //     <Profile
-    //     changeParentState = {this.ChangeAppState}/>
-    //   )
-    // }
