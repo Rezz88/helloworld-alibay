08bca247cc63081eebe85a727476013b1fdc4771
Rezz88 <zzerep.88@hotmail.com>
2018-02-13 11:56:59 -0500

    Perez - Update to mockdata

--- frontend/src/views/profile/Items-Sold.js
+++ frontend/src/views/profile/Items-Sold.js
@@ -2,63 +2,26 @@ import React, { Component } from 'react';
 import '../../App.css';
 
 export class ItemSold extends Component {
-    constructor () {
-        super();
-        this.state = { products: [] }
-    }
 
-       
-    // renderProducts = () => {
-    //     const { products } = this.state
-    //     if (products.length) {
-    //         return products.map(product => {
-    //             return <SoldItem
-    //                 name={product.name}
-    //                 image={product.image}
-    //                 description={product.descr}
-    //                 prodId={product.prodid}
-    //                 key={product.prodid}
-    //                 // addToBag={this.addToBag}
-    //                 // addToFav={() => this.addToFav(product.prodid)}
-    //             />
-    //         })
-    //     } else {
-    //         return <div>nothing</div>
-    //     }
-    // }
-
-    componentDidMount(){
-        const mockproducts = [
-            {prodid: 1,
-                name: 'car',
-                descr: 'description of car',
-                image: 'img of car'},
-            {prodid: 2,
-                name: 'boat',
-                descr: 'description of boat',
-                image: 'img of boat'},
-            {prodid: 3,
-                name: 'shoes',
-                descr: 'description of shoes',
-                image: 'img of shoes'}
-        ]
-        this.setState({ products: mockproducts })
-    };
-    
     render() {
-        // const{ image, name, description, addToBag, prodId, addToFav} = this.state.products
-        return (<div className="sold-item">
-            <div>Image
-                {this.state.products.image}
+        const{ image, price, name, description, addToCart, addToFav} = this.props// de-structuring shit from perks
+        return (
+            <div className='Main-items'>
+                <div>
+                    {image}
+                </div>
+                <div>
+                    {name}
+                </div>
+                <div>
+                    {description}
+                </div>
+                <div>
+                    {price}
+                </div>
             </div>
-            <div>Name
-                {this.state.products.name}
-            </div>
-            <div>Description
-                {this.state.products.description}
-            </div>
-        </div>
         )
     }
-}
+}      
+
 export default ItemSold;
--- frontend/src/views/profile/index.js
+++ frontend/src/views/profile/index.js
@@ -14,6 +14,43 @@ export class Profile extends Component {
             products: []
         }
     }
+    
+    renderProducts = () => {
+        const { products } = this.state
+        if (products.length) {
+            return products.map(product => {
+                return <SoldItem
+                    name={product.name}
+                    image={product.image}
+                    description={product.descr}
+                    prodId={product.prodid}
+                    key={product.prodid}
+                    // addToBag={this.addToBag}
+                    // addToFav={() => this.addToFav(product.prodid)}
+                />
+            })
+        } else {
+            return <div>nothing</div>
+        }
+    }
+
+    componentDidMount(){
+        const mockproducts = [
+            {prodid: 1,
+                name: 'car',
+                descr: 'description of car',
+                image: 'img of car'},
+            {prodid: 2,
+                name: 'boat',
+                descr: 'description of boat',
+                image: 'img of boat'},
+            {prodid: 3,
+                name: 'shoes',
+                descr: 'description of shoes',
+                image: 'img of shoes'}
+        ]
+        this.setState({ products: mockproducts })
+    };
 
     renderComponent = () => {
         const {active} = this.state;
