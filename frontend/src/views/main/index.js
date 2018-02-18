48997bda4f3aa9cfa55f00f794d1f8f7ff819c36
Rezz88 <zzerep.88@hotmail.com>
2018-02-18 14:58:55 -0500

    Perez - Added style to all buttons and modified logout

--- frontend/src/views/main/index.js
+++ frontend/src/views/main/index.js
@@ -230,18 +230,18 @@ export class Main extends Component {
                         <button id="sort" onClick={this.sortTime}>recent</button>
                     </div>
                     <div>
-                    <button className="tableOfContents" onClick={this.categotySearch}>Clear</button> 
-                    <button id="tableOfContents" onClick={this.categotySearch}>Artisanal</button>
-                    <button id="tableOfContents" onClick={this.categotySearch}>Audio</button>
-                    <button id="tableOfContents" onClick={this.categotySearch}>Automotive</button>
-                    <button id="tableOfContents" onClick={this.categotySearch}>Beauty and Health</button>
-                    <button id="tableOfContents" onClick={this.categotySearch}>Books/Audible</button>
-                    <button id="tableOfContents" onClick={this.categotySearch}>Clothing</button>
-                    <button id="tableOfContents" onClick={this.categotySearch}>Electronics</button>
-                    <button id="tableOfContents" onClick={this.categotySearch}>Home, Garden and Tools</button>
-                    <button id="tableOfContents" onClick={this.categotySearch}>Toys, Kids and Baby</button>
-                    <button id="tableOfContents" onClick={this.categotySearch}>Odd Jobs</button>
-                    <button id="tableOfContents" onClick={this.categotySearch}>Other</button>
+                    <button className ="button2" id="tableOfContents" onClick={this.categotySearch}>Clear</button> 
+                    <button className ="button2" id="tableOfContents" onClick={this.categotySearch}>Artisanal</button>
+                    <button className ="button2" id="tableOfContents" onClick={this.categotySearch}>Audio</button>
+                    <button className ="button2" id="tableOfContents" onClick={this.categotySearch}>Automotive</button>
+                    <button className ="button2" id="tableOfContents" onClick={this.categotySearch}>Beauty and Health</button>
+                    <button className ="button2" id="tableOfContents" onClick={this.categotySearch}>Books/Audible</button>
+                    <button className ="button2" id="tableOfContents" onClick={this.categotySearch}>Clothing</button>
+                    <button className ="button2" id="tableOfContents" onClick={this.categotySearch}>Electronics</button>
+                    <button className ="button2" id="tableOfContents" onClick={this.categotySearch}>Home, Garden and Tools</button>
+                    <button className ="button2" id="tableOfContents" onClick={this.categotySearch}>Toys, Kids and Baby</button>
+                    <button className ="button2" id="tableOfContents" onClick={this.categotySearch}>Odd Jobs</button>
+                    <button className ="button2" id="tableOfContents" onClick={this.categotySearch}>Other</button>
                     </div>
                     <div>
                         {this.renderProducts()}
